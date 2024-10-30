d6dd1282d46b4f427e9a179b242a2e68574685e0
Matheus Parizotto <93141291+MatheusParizotto@users.noreply.github.com>
2024-10-30 17:22:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template # type: ignore
 import tensorflow as tf
 import sqlite3
 from PIL import Image
 import numpy as np
 import os
+from tensorflow.keras.losses import MeanSquaredError
 
 app = Flask(__name__)
+
+# Carregar o modelo autoencoder 
+try:
+    autoencoder = tf.keras.models.load_model(r'C:/Users/Parizotto/OneDrive/Documentos/Classificador_de_plantas/autoencoder_flores.h5', custom_objects={'mse': MeanSquaredError()})
+except Exception as e:
+    print(f"Erro ao carregar o autoencoder: {e}")
 
 # Carregar o modelo 
 try:
     model = tf.keras.models.load_model(r'C:/Users/Parizotto/OneDrive/Documentos/Classificador_de_plantas/classificador_de_flores.h5')
 except Exception as e:
-    print(f"Erro ao carregar o modelo: {e}")
+    print(f"Erro ao carregar o modelo de classificação: {e}")
 
 # Caminho para o upload de imagens
 UPLOAD_FOLDER = 'static/uploads'
@@ -34,46 +41,44 @@ def index():
             image = np.array(image) / 255.0
             image = np.expand_dims(image, axis=0)
 
-            # Mapeamento
+            # Mapeamento de classes
             class_mapping = {0: 'Dente-de-leão', 1: 'Girassol', 2: 'Margarida', 3: 'Rosa', 4: 'Tulipa'}
 
+            reconstructed_image = autoencoder.predict(image)
+            loss = np.mean(np.abs(image - reconstructed_image))
 
-            # Predição
-            predictions = model.predict(image)
-            classe_planta = np.argmax(predictions[0])
+            limiar_anomalia = 0.02
 
-            nome_planta = class_mapping[classe_planta]
+            if loss > limiar_anomalia:
+                nome_planta = "Desconhecida"
+                cuidados_necessarios = "Informações não disponíveis."
+            else:
+                # Predição
+                predictions = model.predict(image)
+                confidence = np.max(predictions[0])
+                classe_planta = np.argmax(predictions[0])
 
-            print("Classe Predita (índice):", classe_planta)
+                limiar_confiança = 0.9
 
-            try:
-                conn = sqlite3.connect('data/cuidados.db')
-                cursor = conn.cursor()
-                print("Conexão com o banco de dados estabelecida com sucesso!")
-                
-                # Verifique se o nome da planta está correto e remova possíveis espaços extras
-                nome_planta = class_mapping[classe_planta].strip()
-
-                # Faça a consulta no banco de dados
-                cursor.execute("SELECT cuidados_necessarios FROM instrucoes WHERE nome_planta = ?", (nome_planta,))
-                resultado = cursor.fetchone()
-
-                if resultado:
-                    cuidados_necessarios = resultado[0]
-                    print(f"Nome da Planta: {nome_planta}")
-                    print(f"Cuidados Necessários: {cuidados_necessarios}")
+                if confidence < limiar_confiança:
+                    nome_planta = "Desconhecida"
+                    cuidados_necessarios = "Informações não disponíveis."
                 else:
-                    print("Nome da Planta: Desconhecida")
-                    print("Cuidados Necessários: Informações não disponíveis.")
-                    
-            except sqlite3.Error as e:
-                print(f"Erro ao conectar ao banco de dados: {e}")
-                nome_planta = "Erro"
-                cuidados_necessarios = "Não foi possível acessar o banco de dados."
-                
-            print("Resultado", resultado)
-            print("Nome da Planta:", nome_planta)
-            print("Cuidados Necessários:", cuidados_necessarios)
+                    nome_planta = class_mapping[classe_planta]
+                    # Conectar ao banco de dados
+                    try:
+                        conn = sqlite3.connect('data/cuidados.db')
+                        cursor = conn.cursor()
+                        cursor.execute("SELECT cuidados_necessarios FROM instrucoes WHERE nome_planta = ?", (nome_planta,))
+                        resultado = cursor.fetchone()
+                        if resultado:
+                            cuidados_necessarios = resultado[0]
+                        else:
+                            cuidados_necessarios = "Informações não disponíveis."
+                    except sqlite3.Error as e:
+                        print(f"Erro ao conectar ao banco de dados: {e}")
+                        nome_planta = "Erro"
+                        cuidados_necessarios = "Não foi possível acessar o banco de dados."
 
             return render_template('index.html', image_path=image_path, nome_planta=nome_planta, cuidados_necessarios=cuidados_necessarios)
     
