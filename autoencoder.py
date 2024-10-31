c4dee4e959f01b7abe84a1ba3bc4f8d1994ed28d
Matheus Parizotto <93141291+MatheusParizotto@users.noreply.github.com>
2024-10-31 14:45:49 -0400

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -3,8 +3,8 @@ from tensorflow.keras import layers, models # type: ignore
 from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
 
 # Caminhos para os dados de treino e validação
-train_dir = r'C:/Users/Parizotto/OneDrive/Documentos/Classificador_de_plantas/data/train'
-val_dir = r'C:/Users/Parizotto/OneDrive/Documentos/Classificador_de_plantas/data/validation'
+train_dir = 'C:/Users/mathe/OneDrive/Documentos/Identificador-de-Flores/data/train'
+validation_dir = 'C:/Users/mathe/OneDrive/Documentos/Identificador-de-Flores/data/validation'
 
 # Data Augmentation
 train_datagen = ImageDataGenerator(rescale=1./255)
@@ -14,11 +14,11 @@ train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(224, 224),
     batch_size=32,
-    class_mode='input'  # Usamos 'input' porque no autoencoder a entrada é igual à saída
+    class_mode='input'  
 )
 
 val_generator = val_datagen.flow_from_directory(
-    val_dir,
+    validation_dir,
     target_size=(224, 224),
     batch_size=32,
     class_mode='input'
@@ -26,7 +26,7 @@ val_generator = val_datagen.flow_from_directory(
 
 # Autoencoder
 input_shape = (224, 224, 3)
-encoding_dim = 64  # Você pode ajustar conforme necessário
+encoding_dim = 64  
 
 # Encoder
 input_img = layers.Input(shape=input_shape)
@@ -53,7 +53,7 @@ autoencoder.fit(
     steps_per_epoch=train_generator.samples // train_generator.batch_size,
     validation_data=val_generator,
     validation_steps=val_generator.samples // val_generator.batch_size,
-    epochs=20  # Ajuste conforme necessário
+    epochs=30  
 )
 
 # Salvando o Autoencoder treinado
